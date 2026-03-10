7fbb9cf8a67cfdc660da000b83fecb33463a0a7f
Tmonster <tom@ebergen.com>
2026-03-10 12:28:53 +0100

    fix avro port

--- vcpkg_ports/avro-c/portfile.cmake
+++ vcpkg_ports/avro-c/portfile.cmake
@@ -1,28 +1,28 @@
 vcpkg_buildpath_length_warning(37)
 if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+  vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 endif()
 
 
 set(OPTIONAL_DUCKDB_PATCHES "")
 if (VCPKG_TARGET_IS_EMSCRIPTEN)
-   set(CMAKE_POSITION_INDEPENDENT_CODE ON)
-   set(CMAKE_CXX_FLAGS " -fPIC ${VCPKG_CXX_FLAGS}" CACHE STRING "")
-   set(CMAKE_C_FLAGS " -fPIC ${VCPKG_C_FLAGS}" CACHE STRING "")
+  set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+  set(CMAKE_CXX_FLAGS " -fPIC ${VCPKG_CXX_FLAGS}" CACHE STRING "")
+  set(CMAKE_C_FLAGS " -fPIC ${VCPKG_C_FLAGS}" CACHE STRING "")
 
-   set(IS_CROSS_COMPILE 1)
-   set(cross_compiling 1)
-   set(VCPKG_CROSSCOMPILING 1)
+  set(IS_CROSS_COMPILE 1)
+  set(cross_compiling 1)
+  set(VCPKG_CROSSCOMPILING 1)
 
-   set(OPTIONAL_DUCKDB_PATCHES "${ADDITIONAL_PATCHES} static_link_only.patch")
+  set(OPTIONAL_DUCKDB_PATCHES "${ADDITIONAL_PATCHES} static_link_only.patch")
 endif()
 separate_arguments(OPTIONAL_DUCKDB_PATCHES)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO duckdb/duckdb-avro-c
-    REF 4b7d9136180cb1d088d4cdb40eb80813b3774c21
-    SHA512 ffafc3617f032583d8e08ae933009936c8b49b0a9708fefa58462915b185b0f402a741be460a107db4bc5c4467cdfb6b452ea29668f4b609d79c4f20947f123e
+    REF dc6414e65174264195f41bafd936b8f2db7cb843
+    SHA512 884bea221bb3006b25d9e1cd69829ed700044b158a6cdc25ebfc740e03d0ee08139a786f79fe48f2fa9d5f03e3030cddf22dcc5f8dbbd59254e1c5fe7fb65cec
     PATCHES
         ${OPTIONAL_DUCKDB_PATCHES}
 )
@@ -46,15 +46,15 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/pkgconfig" "${CURRENT_PACKAGES_
 
 
 if(NOT VCPKG_TARGET_IS_EMSCRIPTEN)
-    vcpkg_copy_tools(TOOL_NAMES avroappend avrocat AUTO_CLEAN)
+  vcpkg_copy_tools(TOOL_NAMES avroappend avrocat AUTO_CLEAN)
 
-    if(NOT VCPKG_TARGET_IS_WINDOWS)
-        vcpkg_copy_tools(TOOL_NAMES avropipe avromod AUTO_CLEAN)
-    endif()
+  if(NOT VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_copy_tools(TOOL_NAMES avropipe avromod AUTO_CLEAN)
+  endif()
 
-    if(VCPKG_LIBRARY_LINKAGE STREQUAL "static" AND NOT VCPKG_TARGET_IS_WINDOWS)
-        file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-    endif()
+  if(VCPKG_LIBRARY_LINKAGE STREQUAL "static" AND NOT VCPKG_TARGET_IS_WINDOWS)
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+  endif()
 endif()
 
 file(INSTALL "${SOURCE_PATH}/lang/c/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
